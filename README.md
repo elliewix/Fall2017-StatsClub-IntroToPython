4b909fc473d661bd4ea5dfeb7519c3fb600db9cb
Elizabeth Wickes <elliewix@gmail.com>
2017-09-21 15:06:39 -0500

    adding content

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Fall2017-StatsClub-IntroToPython
+# Intro to Python: Illini Statistics Club (Fall 2017)
+
+* 6-7pm, Monday, September 25, 2017
+* 1002 Lincoln Hall 
+
+## Premis
+
+R is a powerful tool for statistics, data anasysis, and data visualization, and can be used for a wide variety of data processing tasks.  Unfortunately, you may end up working on a team that only uses Python, need to use a tool that is only in Python, or just have a task too unwieldy for you to accomplish in R.
+
+No matter the reason, the current state of data science requires that we can be functional in both R and Python.  Many blog posts out there work hard to try and compare the two, and attempt to pick a winner.  Analysis of tools like StackOverflow and GitHub are trying to measure the growth of each over time, but no matter who might be the winner of the day you'll need to know both to some level.
+
+## What we'll cover here
+
+This will be more of a tour of python and going through an example program.
+
+1. What is Python and a bit of the history
+2. The python ecosystem and how to install things
+3. Stepping through a data processing task
+
+This is not meant to be the only python expoluse that you need to get started, but instead to be the 
---
+++ week4demo.py
@@ -0,0 +1,24 @@
+f = open('report.txt', 'r')
+
+full_text = f.read()
+
+f.close()
+
+sample = """Kozuch, Laura; Walker, Karen; Marquardt, William (2017): Modern sinistral whelk spire angles, genus Busycon. University of Illinois at Urbana-Champaign. https://doi.org/10.13012/B2IDB-2031816_V1
+Downloads: 9 (2017-08-30 to 2017-09-13 )
+-----
+
+Park, Jungsik; Le, Brian; Sklenar, Joseph; Chern, Gia-wei; Watts, Justin; Schiffer, Peter (2017): Magnetic response of brickwork artificial spin ice. University of Illinois at Urbana-Champaign. https://doi.org/10.13012/B2IDB-1528275_V1
+Funder: U.S. Department of Energy (DOE), Grant: DE-SC0010778
+Downloads: 10 (2017-09-08 to 2017-09-13 )
+-----
+
+Kozuch, Laura; Walker, Karen; Marquardt, William (2017): Modern sinistral whelk spire angles, genus Busycon . University of Illinois at Urbana-Champaign. https://doi.org/10.13012/B2IDB-2031816_V2
+Downloads: 6 (2017-09-06 to 2017-09-13 )
+-----
+
+Christensen, Sarah; Molloy, Erin K.; Vachaspati, Pranjal; Warnow, Tandy (2017): Datasets from the study: Optimal completion of incomplete gene trees in polynomial time using OCTAL. University of Illinois at Urbana-Champaign. https://doi.org/10.13012/B2IDB-8402610_V1
+Funder: U.S. National Science Foundation (NSF), Grant: CCF-1535977
+Funder: U.S. National Science Foundation (NSF), Grant: DGE-1144245
+Downloads: 47 (2017-06-15 to 2017-09-13 )
+-----"""
